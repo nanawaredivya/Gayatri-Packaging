56c79698654866badaa9135ba5404b399aec3086
nanawaredivya <divyananaware28@gmail.com>
2024-03-26 19:03:11 +0530

    add button

--- gayatri-packaging/src/components/Pages/HomePage/InfoAbout.jsx
+++ gayatri-packaging/src/components/Pages/HomePage/InfoAbout.jsx
@@ -1,11 +1,12 @@
-import { Grid, Typography } from '@mui/material'
+import { Button, Grid, Typography } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const InfoAbout = () => {
     return (
-        <Grid container spacing={2} sx={{backgroundColor: "#E0FFFF"}}>
+        <Grid container spacing={2} sx={{ backgroundColor: "#E0FFFF" }}>
             <Grid item xs={12} md={4}>
-            <img src="/path/to/your/image.jpg" alt="Product Name" style={{ width: '100%', height: 'auto' }} />
+                <img src="/path/to/your/image.jpg" alt="Product Name" style={{ width: '100%', height: 'auto' }} />
             </Grid>
             <Grid item xs={12} md={8}>
                 <Typography variant="body1"
@@ -27,10 +28,20 @@ const InfoAbout = () => {
                     sx={{
                         fontSize: "1rem",
                         marginTop: "1rem",
-                        marginBottom: "2rem"
+                        marginBottom: "0.8rem",
                     }}>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias odit expedita, nemo repellendus tenetur minus animi ipsa ab beatae veritatis officiis tempora, ut ea est eos, aspernatur exercitationem repellat? Illo adipisci iusto beatae tempore accusantium provident facere quasi accusamus nesciunt eligendi, necessitatibus voluptatem ad. Excepturi aspernatur laboriosam a deleniti at.
                 </Typography>
+                <Button
+                    variant="contained"
+                    component={Link}
+                    to="../About"
+                    sx={{ 
+                        marginBottom: "2rem",
+                        borderRadius: "20px", 
+                        }}>
+                    READ MORE
+                </Button>
             </Grid>
         </Grid>
     )
